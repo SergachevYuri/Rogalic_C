54b05453e0677a624e94634cc6b6d3a314130b39
SYuri <sergachev.yuri@gmail.com>
2024-10-19 15:19:33 +0900

    fist init

---
+++ src/rogalic.c
@@ -0,0 +1,63 @@
+#include "rogalic.h"
+
+
+int main() {
+    struct Player player;
+    char* map[HEIGHT][WIDTH];
+    struct Monsters mob;
+    player.x = 5;
+    player.y = 5;
+    player.hp = 10;
+    dungeons(map);
+    mob = Summon_Monsters();
+    graphic(player, map, mob);
+    return 0;
+}
+
+void graphic(struct Player player, char* map[HEIGHT][WIDTH], struct Monsters mob) {
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if(i == player.x && j == player.y){
+                printf("@");
+            } else if(i == mob.x && j == mob.y){
+                printf("%c", mob.pointer);
+            } else {
+                printf("%s", map[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    
+}
+
+void dungeons(char* map[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if((i == 0 && j != 0 && j != WIDTH) || (i == HEIGHT - 1 && j != 0 && j != WIDTH)) {
+                map[i][j] = "#";
+            } else if((i != 0 && i != HEIGHT - 1 && j == 0) || (i != 0 && i != HEIGHT - 1 && j == WIDTH - 1)){
+                map[i][j] = "#";
+            } else {
+                map[i][j] = " ";
+            }
+        }
+        
+    }
+    
+}
+
+struct Monsters Summon_Monsters() {
+    struct Monsters goblin;
+    goblin.x = 20;
+    goblin.y = 20;
+    goblin.pointer = 'g';
+    goblin.hp = 2;
+    goblin.attak = 2;
+    strcpy(goblin.name, "Goblin Moblin");
+
+    return goblin;
+}
---
+++ src/rogalic.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ncurses.h>
+#include <string.h>
+#include "player.h"
+#include "monsters.h"
+
+#define HEIGHT 30
+#define WIDTH 100
+
+
+void graphic(struct Player player, char* map[HEIGHT][WIDTH], struct Monsters mob);
+void dungeons(char* map[HEIGHT][WIDTH]);
+struct Monsters Summon_Monsters();
